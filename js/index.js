13ac301560cd85bea4bce0c86c0bcf626739fa61
Eric <103874060+developwithe2@users.noreply.github.com>
2024-07-29 10:38:37 -0700

    Create index.js

---
+++ js/index.js
@@ -0,0 +1,14 @@
+// Setting string variable
+const text = 'Hello World!';
+
+// Using document.write()
+document.write(text);
+
+// Using DOM manipulation
+document.getElementById('output').innerText = text;
+
+// Using the console
+console.log(text);
+
+// Using an alert
+alert(text);
